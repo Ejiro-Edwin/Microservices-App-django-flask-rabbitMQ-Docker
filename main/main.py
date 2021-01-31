642cfb55aedff750b5f5e492b3810edb1cd0604b
ejiro-edwin <ejiroedwin@gmail.com>
2021-01-31 22:12:08 +0100

    Docker configuration for flask app

---
+++ main/main.py
@@ -0,0 +1,60 @@
+from flask import Flask, jsonify
+import requests
+from dataclasses import dataclass
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import UniqueConstraint
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://dimeji:Password123#@!@dbs/main'
+CORS(app)
+
+db = SQLAlchemy(app)
+
+@dataclass
+class Product(db.Model):
+    id: int
+    title: str
+    image: str
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    title = db.Column(db.String(200))
+    image = db.Column(db.String(200))
+
+@dataclass
+class ProductUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    product_id = db.Column(db.Integer)
+
+    UniqueConstraint('user_id', 'product_id', name='user_product_unique')
+
+@app.route('/api/products')
+def index():
+    return jsonify(Product.query.all())
+
+@app.route('/api/products/<int:id>/like', methods=['POST'])
+def like(id):
+    req = requests.get('http://172.18.0.4:8000/api/user')
+    json = req.json()
+
+    try:
+        productUser = ProductUser(user_id=json['id'], product_id=id)
+        db.session.add(productUser)
+        db.session.commit()
+
+        publish('product_liked', id)
+    except:
+        abort(400, 'You already liked this product')
+
+    return jsonify({
+        'message': 'success'
+    })
+
+
+@app.route('/')
+def index():
+    return "Hello"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
